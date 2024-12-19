7aabad0dd2f93a71699611cf977a2fedad55a510
Frans Ekman <fransedvinekman@gmail.com>
2024-12-19 10:59:19 +0100

    astro v5 migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fransekman.vercel.app/',
   integrations: [sitemap(), tailwind()],
-  output: 'hybrid',
-  adapter: vercel()
-});
+  output: 'static',
+  adapter: vercel(),
+})
